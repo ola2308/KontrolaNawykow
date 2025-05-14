cdd37869fd653ee9019d3722ae6a8d75c735cd17
Marvee2202 <115485515+Marvee2202@users.noreply.github.com>
2025-05-14 22:54:04 +0200

    Podstawowa struktura panelu administracyjnego
    Jeszcze nic nie robi; weryfikacja statusu administratora na razie zakomentowana dla wygody testowania

---
+++ Pages/Admin/Reports.cshtml.cs
@@ -0,0 +1,60 @@
+using KontrolaNawykow.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace KontrolaNawykow.Pages.Admin
+{
+    public class ReportsModel : PageModel
+    {
+        public User CurrentUser { get; set; }
+
+        private readonly ApplicationDbContext _context;
+        public ReportsModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            try
+            {
+                // Pobierz ID zalogowanego uøytkownika
+                if (!User.Identity.IsAuthenticated)
+                {
+                    return RedirectToPage("/Account/Login");
+                }
+
+                var userIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                if (string.IsNullOrEmpty(userIdClaim) || !int.TryParse(userIdClaim, out int userId))
+                {
+                    return RedirectToPage("/Account/Login");
+                }
+
+                // Pobierz dane uøytkownika
+                CurrentUser = await _context.Users
+                    .FirstOrDefaultAsync(u => u.Id == userId);
+
+                if (CurrentUser == null)
+                {
+                    return RedirectToPage("/Account/Login");
+                }
+
+                // Sprawdü, czy uøytkownik jest administratorem
+                //if (CurrentUser.Admin == null)
+                //{
+                //    return RedirectToPage("/Diet/Index");
+                //}
+
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                // Logowanie b≥Ídu
+                Console.WriteLine($"B≥πd podczas ≥adowania strony diety: {ex.Message}");
+                return RedirectToPage("/Error", new { message = ex.Message });
+            }
+        }
+    }
+}
