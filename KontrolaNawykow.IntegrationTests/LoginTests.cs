e196328918378a78f7a065d5e4104a35e2caf454
Marvee2202 <115485515+Marvee2202@users.noreply.github.com>
2025-07-25 02:20:54 +0200

    Próba zdobycia tokenu w testach

--- KontrolaNawykow.IntegrationTests/LoginTests.cs
+++ KontrolaNawykow.IntegrationTests/LoginTests.cs
@@ -2,7 +2,10 @@
 
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Runtime.InteropServices.JavaScript;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using Xunit;
 
 public class LoginTests : IClassFixture<CustomWebApplicationFactory>
@@ -32,22 +35,69 @@ public class LoginTests : IClassFixture<CustomWebApplicationFactory>
     [Fact]
     public async Task Login_WithValidCredentials_ShouldRedirect()
     {
+        //Wydobycie tokenu
+        var formPage = await _client.GetAsync("/Account/Login");
+
+        Assert.Equal(System.Net.HttpStatusCode.OK, formPage.StatusCode);
+
+        var form = await formPage.Content.ReadAsStringAsync();
+
+        Regex tokenSearch = new Regex("<input name=\"__RequestVerificationToken\".*\\/>");
+
+        var tokenField = tokenSearch.Matches(form).FirstOrDefault();
+        Assert.NotNull(tokenField);
+        
+        var token = tokenField.Value;
+
+        Regex valueSearch = new Regex("value=\".*\" />");
+
+        var valueField = valueSearch.Matches(token).FirstOrDefault();
+        Assert.NotNull(valueField);
+
+        var value = valueField.Value;
+        value = value.Substring(7, value.Length - 11);
+        //Wydobycie tokenu - koniec
+
         var formData = new FormUrlEncodedContent(new[]
         {
-            new KeyValuePair<string, string>("__RequestVerificationToken", "???"),
+            new KeyValuePair<string, string>("__RequestVerificationToken", value),
             new KeyValuePair<string, string>("Username", "testuser"),
             new KeyValuePair<string, string>("Password", "Test123!")
         });
 
         var response = await _client.PostAsync("/Account/Login", formData);
 
-        Assert.Equal(System.Net.HttpStatusCode.Redirect, response.StatusCode);
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
         Assert.Equal("/Diet/Index", response.Headers.Location.ToString());
     }
 
     [Fact]
     public async Task Login_WithInvalidCredentials_ShouldShowError()
     {
+
+        //Wydobycie tokenu
+        var formPage = await _client.GetAsync("/Account/Login");
+
+        Assert.Equal(System.Net.HttpStatusCode.OK, formPage.StatusCode);
+
+        var form = await formPage.Content.ReadAsStringAsync();
+
+        Regex tokenSearch = new Regex("<input name=\"__RequestVerificationToken\".*\\/>");
+
+        var tokenField = tokenSearch.Matches(form).FirstOrDefault();
+        Assert.NotNull(tokenField);
+
+        var token = tokenField.Value;
+
+        Regex valueSearch = new Regex("value=\".*\" />");
+
+        var valueField = valueSearch.Matches(token).FirstOrDefault();
+        Assert.NotNull(valueField);
+
+        var value = valueField.Value;
+        value = value.Substring(7, value.Length - 11);
+        //Wydobycie tokenu - koniec
+
         var formData = new FormUrlEncodedContent(new[]
         {
             new KeyValuePair<string, string>("__RequestVerificationToken", "???"),
